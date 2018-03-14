35a67b98cdf8559f5fc162f83d555531824d90d6
吴芳敏 <80002748@sf-express.com>
2018-03-14 19:19:40 +0800

    add around advice

--- src/main/java/com/example/demo/aspect/HttpAspect.java
+++ src/main/java/com/example/demo/aspect/HttpAspect.java
@@ -1,5 +1,7 @@
 package com.example.demo.aspect;
 
+import com.example.demo.enums.ExceptionEnum;
+import com.example.demo.util.ResultUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -60,27 +62,16 @@ public class HttpAspect {
         logger.info("response={}", object.toString());
     }
 
-//    @Around("log()") // 前置+后置通知 改为环绕通知
-//    public void doAround(ProceedingJoinPoint proceedingJoinPoint,JoinPoint joinPoint){
-//        try {
-//            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//            HttpServletRequest request = attributes.getRequest();
-//
-//            logger.info("url={}", request.getRequestURI());
-//
-//            logger.info("method={}", request.getMethod());
-//
-//            logger.info("ip={}", request.getRemoteAddr());
-//
-//            logger.info("class_method={}", joinPoint.getSignature().getDeclaringType() + "." + joinPoint.getSignature().getName());
-//
-//            logger.info("args={}", joinPoint.getArgs());
-//
-//            proceedingJoinPoint.proceed();
-//
-//            logger.info("doAfter method executing");
-//        } catch (Throwable e) {
-//            logger.error("around advice exception"+e);
-//        }
-//    }
+    @Around("log()") // 前置+后置通知 改为环绕通知,proceed()处理完连接点方法后还需要返回调用结果
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint){
+        try {
+            logger.info("before method executing");
+            Object obj = proceedingJoinPoint.proceed();
+            logger.info("after method executing");
+            return obj;
+        } catch (Throwable e) {
+            logger.error("around advice exception"+e);
+        }
+        return ResultUtil.error(ExceptionEnum.UNKNOW_ERROR);
+    }
 }
